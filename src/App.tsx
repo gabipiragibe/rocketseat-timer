4ec8d881bbed19431d8278274e0ec98361373af3
Gabriela Piragibe <piragibegabriela@gmail.com>
2024-11-12 18:20:48 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/themes/default"
+export const App = ( ) => {
+  return (
+    <ThemeProvider theme={defaultTheme}> 
+      <button></button>
+      <button></button>
+    </ThemeProvider>
+   
+  )
+}
