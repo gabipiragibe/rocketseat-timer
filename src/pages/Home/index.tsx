67af620ab831a24d011f5d206cb8d6e45386f6a6
Gabriela Piragibe <piragibegabriela@gmail.com>
2024-12-09 19:21:10 -0300

    feat: Add form validation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,40 @@
 import { Play } from "phosphor-react";
+import { useForm } from "react-hook-form";
 import * as S from "./styles";
+import * as zod from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, "Informe a tarefa"),
+  minutesAmount: zod
+    .number()
+    .min(5, "O cliclo precisa ser de no mínimo 5 minutos.")
+    .max(60, "O ciclo precisa ser no máximo 60 minutos."),
+});
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
 
 export const Home = () => {
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: "",
+      minutesAmount: 0,
+    },
+  });
+  //register add um input ao form, fala quais campos vou ter no form
+
+  const handleCreateNewCycle = (data: NewCycleFormData) => {
+    console.log(data);
+    reset();
+  };
+
+  const task = watch("task");
+  const isSubmitDisabled = !task;
+
   return (
     <S.HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <S.FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           {/* label para que quando a pessoa clique aqui de focus no input */}
@@ -12,6 +42,7 @@ export const Home = () => {
             id="task"
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
+            {...register("task")}
           />
 
           <datalist id="task-suggestions">
@@ -28,6 +59,7 @@ export const Home = () => {
             step={5}
             min={5}
             max={60}
+            {...register("minutesAmount", { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
@@ -41,7 +73,7 @@ export const Home = () => {
           <span>0</span>
         </S.CountdownContainer>
 
-        <S.StartCountdownButton type="submit">
+        <S.StartCountdownButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           Começar
         </S.StartCountdownButton>
